5816161eedc066aac77109a4943eec3f3f7cccdf
AGL_Hồ Sỹ Huy  (フイ) <117057469+syhuyagl@users.noreply.github.com>
2023-12-18 16:14:42 +0700

    update

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,13 +1,13 @@
 // Initialize Slick
 $(document).ready(function () {
-  var swiper = new Swiper('.swiper-container', {
+  var swiper = new Swiper(".swiper-container", {
     autoplay: {
-      delay: 1500
+      delay: 1500,
     },
     slidesPerView: 1,
 
     speed: 5000,
-    effect: 'fade',
+    effect: "fade",
     fadeEffect: {
       crossFade: true, // Enable cross-fade
     },
@@ -15,9 +15,15 @@ $(document).ready(function () {
       delay: 1000, // Autoplay delay in milliseconds
     },
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
       clickable: true,
     },
-  
+  });
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= 176) {
+      $("nav").addClass("fixed-header");
+    } else {
+      $("nav").removeClass("fixed-header");
+    }
   });
 });
